0281bd1782f96d6f27eace9215729c3c96750122
maiiku <mko_san@lafiel.net>
2018-08-11 00:58:46 +0000

    changes

--- loire/loire/settings/base.py
+++ loire/loire/settings/base.py
@@ -159,4 +159,5 @@ WAGTAIL_SITE_NAME = "loire"
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-BASE_URL = 'http://example.com'
+BASE_URL = 'http://loire.lafiel.net'
+ALLOWED_HOSTS = ['loire.lafiel.net', 'www.loire.lafiel.net']
